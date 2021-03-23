369ce380ab180a386046ce1e77ba25f5074d4b18
Tushar Takshak <66051224+trixtun@users.noreply.github.com>
2021-03-23 09:55:34 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,39 +1,4 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-   int n[7] = {2,3,4,5,6,7,8};
-   int N[7];
-   for(int i=0;i<7;i++)
-   {
-       N[i] = n[6-i];
-   }
-
-   for(int j=0;j<7;j++)
-   {
-        cout<<N[j]<< " ";
-   }
-
-}
-
-
-
-
-//Alternate methods  :   https://www.javatpoint.com/cpp-algorithm-reverse-function
-
-
-
-
-
-
-
 //for string reverse
-
-
-
-/*
 #include <algorithm>  
 #include <iostream>  
 #include <string>  
@@ -51,8 +16,9 @@ int main() {
 }  
 
 
+//Alternate methods  :   https://www.javatpoint.com/cpp-algorithm-reverse-function
 
-*/
+
 
 
 
